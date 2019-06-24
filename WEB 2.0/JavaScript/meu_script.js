ecd73a44f36e8a91f1b6f8356ffe1fcb401b1fe2
Amaury AC <amaury.costa@hotmail.com>
2019-06-24 11:17:01 -0300

    Adicionando arquivos ao repositório

---
+++ WEB 2.0/JavaScript/meu_script.js
@@ -0,0 +1,7 @@
+//alert('Olá, seja bem vindo ao curso!!!');
+
+/*
+	Selecionar um elemento no DOM
+	Atualizar o vlaor desse elemento com uma string
+*/
+document.getElementById('nome').value = 'Oi';
